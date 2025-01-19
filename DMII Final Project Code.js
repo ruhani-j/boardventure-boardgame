a66329e702bf84365bb7d3d75f0751054b7e7c00
ruhani-j <ruhanijindal@gmail.com>
2025-01-19 16:58:40 -0600

    Add initial game board - basic p5.js setup, snake-path board layout, sky blue background, sandy squares, START/FINISH labels (Jolomi)

---
+++ DMII Final Project Code.js
@@ -0,0 +1,115 @@
+function setup() {
+
+createCanvas(400, 400);
+
+}
+
+function draw() {
+
+background(220)
+
+// make grid lines -sky blueish background and grid lines darker blue
+
+// sandy beige pathway with red stop signs
+
+// pathway needs boundaries
+
+function setup() {
+
+createCanvas(500, 400);
+
+}
+
+function draw() {
+
+background('skyblue');
+
+strokeWeight(2);
+
+fill(246,215,176)
+
+stroke(62, 103, 214)
+
+square(370,50,60);
+
+square(310,50,60);
+
+square(250,50,60);
+
+square(190,50,60);
+
+square(130,50,60);
+
+square(70,50,60);
+
+square(10,50,60);
+
+square(10,110,60);
+
+square(10,170,60);
+
+square(70,170,60);
+
+square(130,170,60);
+
+square(190,170,60);
+
+square(250,170,60);
+
+square(310,170,60);
+
+square(370,170,60);
+
+square(370,230,60);
+
+square(370,290,60);
+
+square(310,290,60);
+
+square(250,290,60);
+
+square(190,290,60);
+
+square(130,290,60);
+
+square(70,290,60);
+
+square(10,290,60);
+
+noStroke()
+
+fill('black')
+
+text('START',375,85)
+
+text('FINISH',15,325)
+
+}
+
+I made a very basic game board the should be a good base to start off
+of.
+
+// make players
+
+// background music
+
+// make logic for random spinners
+
+// when they enter the game, prompt how many players with buttons
+
+// when they spin make the player move that many spaces
+
+// when they land on a space, make action cards pop up
+
+// make action cards - approx 10 of them similar to game of life action
+cards
+
+// then those action cards need logic to work
+
+// have the players current money total in a designated place
+
+// repeat until game ends
+
+// make
+
+}
